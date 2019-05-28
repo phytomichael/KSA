9904451f21b90c22b949201ad902cfa9963f289c
phytomichael <49068331+phytomichael@users.noreply.github.com>
2019-05-28 09:56:23 +0100

    tesing
    dfdf

--- scan.bat
+++
@@ -1,5 +0,0 @@
-"c:\program files\java\jdk1.8.0_181\bin\java" -jar wss-unified-agent-19.3.1.jar -d C:\Users\phyto\Documents\Source\KSA\ksa -analyzeMultiModule KSAMulti 
-pause
-"c:\program files\java\jdk1.8.0_181\bin\java" -jar xModuleAnalyzer-19.2.2.jar -xModulePath KSAMulti.txt -fsaJarPath wss-unified-agent-19.3.1.jar -c whitesource-fs-agent.config -statusDisplay dynamic
-pause
--analyzeMultiModuleExclusions
